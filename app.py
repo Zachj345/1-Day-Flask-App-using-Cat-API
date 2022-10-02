c2488f0783e4ad4d6d6f2adf786e2bcaf8b85728
Zachary Jordan <zackj345@gmail.com>
2022-10-02 12:16:19 -0400

    just making saves

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ import hashlib
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'a81931dfc45081bda33fcc56872ba14e3de672ebdcf0749cbbbdb6fb9243cb2c'
 
-print(hashlib.sha256('secretye'.encode()).hexdigest())
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
