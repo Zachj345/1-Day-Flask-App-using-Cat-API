f04f06e684d0e3e9e0312a716d4ab610085c8692
Zachary Jordan <zackj345@gmail.com>
2022-10-02 12:46:16 -0400

    getting api key and light html work

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'a81931dfc45081bda33fcc56872ba14e3de672ebdcf0749cbbbdb6fb9243cb2c'
 
+API_KEY = 'live_slwstxVNRlpVUWvVsYjBRMYCCyHR511ixrffrbKmjmLULEYgKNwe9xsGhH4NhqIh'
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def index():
+
     return render_template('index.html')
 
 
